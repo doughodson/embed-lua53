07b98e244bd7d43948d38633cf22687f93ca30ea
Doug Hodson <doug@openeaagles.org>
2019-12-08 14:54:49 -0500

    added scripts to build Makefiles for linux

--- build/premake/run-premake.cmd
+++ build/premake/run-premake.cmd
@@ -4,10 +4,7 @@ rmdir /S /Q ..\gmake
 rmdir /S /Q ..\vs2019
 rmdir /S /Q ..\obj
 rem
-rem delete old compiled libraries
-rem
-del ..\..\lib\*.lib
-rem
 rem create new solution/workspace files
 rem
 premake5_alpha14.exe --os=windows --file=premake5.lua vs2019
+
