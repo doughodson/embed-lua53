c8f787cdd3838d5c17b9744e0b3eaf234c3f002a
Doug Hodson <doug@sidechannel.net>
2019-12-21 15:27:59 -0500

    removed lua 5.1 code

--- repl51/test_function.lua
+++
@@ -1,9 +0,0 @@
-Pythagoras = function(a, b)
-   print "hello, world"
-   return (a*a) + (b*b), a, b
-end
-
-print "This is outside of function call"
-print "call function"
-Pythagoras(3,4)
-print "done"
